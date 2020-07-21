c8e1c75ee50a7b1259fba823ba6d7e6da79d8029
Matheus Eduardo Barboza Santos <matheus.eduardo09@outlook.com>
2020-07-21 20:01:10 -0300

    backup old api

---
+++ api/services/tmdb-old/types.ts
@@ -0,0 +1,14 @@
+interface TypesInterface {
+  movie: string,
+  tv: string
+}
+
+export const TYPES: TypesInterface = {
+  movie: 'movie',
+  tv: 'tv'
+};
+
+export const RESOURCE_URI: TypesInterface = {
+  movie: 'movie',
+  tv: 'tv'
+};
